472323854ed5d88ea03092d9948aa36a8f90debe
Axel <axosar@gmail.com>
2021-01-18 18:56:03 -0600

    Card estilos

--- src/components/users.js
+++ src/components/users.js
@@ -10,10 +10,20 @@ class users extends Component {
     const { users } = this.props.users
 
     return (
-      <div>
+      <div className="cards">
         {users.map(u => 
           <React.Fragment key={u.id}>
-              <h6 >{u.first_name}, {u.last_name}</h6> 
+            <div className="card">
+              <div className="imagen">
+                <img src={u.avatar} alt={u.first_name} />
+              </div>
+              <h6 ><span class="lnr lnr-arrow-right"></span> {u.first_name}, {u.last_name}</h6> 
+              <p>{u.email}</p>
+              <div className="iconos">
+                <span class="lnr lnr-pencil"></span>
+                <span class="lnr lnr-cross"></span>
+              </div>
+            </div>
           </React.Fragment>
         )}
       </div>
